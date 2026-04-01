4b1b341aa81fd4baeea0a663e5d28ddc7f322589
Stef van Buuren <stef.vanbuuren@tno.nl>
2026-04-01 16:07:25 +0200

    Update slides_02

---
+++ R/mice.impute.normdump.R
@@ -0,0 +1,6 @@
+mice.impute.normdump <- function(y, ry, x, ...) {
+    x <- cbind(1, as.matrix(x))
+    parm <- mice:::.norm.draw(y, ry, x, ...)
+    betadump <<- c(betadump, parm$beta)
+    return(x[!ry, ] %*% parm$beta + rnorm(sum(!ry)) * parm$sigma)
+}
